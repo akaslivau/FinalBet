ae66499af3f3f57d079ef30a95f7203132d22f84
Andrew <andrew30511@gmail.com>
2020-04-03 16:18:05 +0400

    03-04-20? ready to code

--- FinalBet/FinalBet/App.xaml.cs
+++ FinalBet/FinalBet/App.xaml.cs
@@ -2,9 +2,14 @@
 using System.Collections.Generic;
 using System.Configuration;
 using System.Data;
+using System.Globalization;
 using System.Linq;
+using System.Threading;
 using System.Threading.Tasks;
 using System.Windows;
+using FinalBet.Database;
+using FinalBet.Properties;
+using FinalBet.ViewModel;
 
 namespace FinalBet
 {
@@ -13,5 +18,42 @@ namespace FinalBet
     /// </summary>
     public partial class App : Application
     {
+        protected override void OnStartup(StartupEventArgs e)
+        {
+            var culture = new CultureInfo("ru-RU")
+            {
+                DateTimeFormat = { ShortDatePattern = "dd.MM.yyyy", LongTimePattern = "" }
+            };
+
+            Thread.CurrentThread.CurrentCulture = culture;
+            Thread.CurrentThread.CurrentUICulture = culture;
+
+            base.OnStartup(e);
+            //Connection.Initialize(Settings.Default.Connection_String);
+
+
+            var window = new MainWindow();
+
+            // Create the ViewModel to which 
+            // the main window binds.
+            var viewModel = new MainWindowViewModel();
+
+            // When the ViewModel asks to be closed, 
+            // close the window.
+            EventHandler handler = null;
+            handler = delegate
+            {
+                viewModel.RequestClose -= handler;
+                window.Close();
+            };
+            viewModel.RequestClose += handler;
+
+            // Allow all controls in the window to 
+            // bind to the ViewModel by setting the 
+            // DataContext, which propagates down 
+            // the element tree.
+            window.DataContext = viewModel;
+            window.Show();
+        }
     }
 }
