8b1e32bbef3399d6a8d660ebb09259bbd934b505
Andrew <andrew30511@gmail.com>
2020-05-02 21:48:01 +0400

    02-05-20 final

--- FinalBet/FinalBet/Model/MatchSolver.cs
+++ FinalBet/FinalBet/Model/MatchSolver.cs
@@ -8,20 +8,42 @@ namespace FinalBet.Model
 {
     public static class MatchSolver
     {
-        public static Output Solve(IMatch match)
+        //Если вдруг будет тормозить
+        private static Dictionary<string, Output> _cachedOutputs = new Dictionary<string, Output>();
+
+        public static Output Solve(IMatch match, SolveMode mode, bool useCache = false)
         {
-            return Output.Nan;
+            //Тут жесткая привязка к режимам, че поделаешь
+            var num = mode.SelectedMode.number;
+
+            //Total
+            if (num == 0)
+                return match.Total > mode.ModeParameter
+                    ? Output.Win
+                    : (match.Total < mode.ModeParameter ? Output.Lose : Output.Deuce);
+
+            //Fora
+            if (num == 1)
+            {
+
+            }
+
+
+            return Output.Na;
         }
     }
 
     public interface IMatch
     {
+        bool IsHome { get; }
+
         int Scored { get; }
         int Missed { get; }
 
         int Total { get; }
         int Dif { get; }
         
+
     }
 
     public enum Output
